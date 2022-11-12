e932bb66d4920b4cca2c10e0dd9836df37a5c752
Defmc <defmc0101@gmail.com>
2022-11-12 09:52:17 -0300

    fix: format `push` script

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
-pwd= $(pwd)
+pwd="$(pwd)"
 pkg=$1
 vers=$2
-cd $pkg
+cd "$pkg"
 docker build -t "defmc/$pkg:latest" .
 docker build -t "defmc/$pkg:$vers" .
-docker push  "defmc/$pkg:latest"
+docker push "defmc/$pkg:latest"
 docker push "defmc/$pkg:$vers"
-cd $pwd
+cd "$pwd"
