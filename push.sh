8019fb15c2147897d5e3d1363101915e43062321
Defmc <defmc0101@gmail.com>
2023-09-09 16:22:02 -0400

    feat(scripts): use `docker-buildx` instead legacy `docker-build`

--- push.sh
+++ push.sh
@@ -8,8 +8,8 @@ pwd="$(pwd)"
 pkg=$1
 vers=$2
 cd "$pkg"
-docker build -t "defmc/$pkg:latest" .
-docker build -t "defmc/$pkg:$vers" .
+docker buildx build -t "defmc/$pkg:latest" .
+docker buildx build -t "defmc/$pkg:$vers" .
 docker push "defmc/$pkg:latest"
 docker push "defmc/$pkg:$vers"
 cd "$pwd"
